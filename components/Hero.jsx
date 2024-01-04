b3e20531daacce87d1c8f855cc8eee245ec3a865
katyaandrushko <katyaandrushko14@gmail.com>
2024-01-04 12:45:29 +0200

    Add Badge

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
    return (
       <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
          <div className="container mx-auto">
-            <div className="flex justify-between gap-x-8">
+            <div className="flex flex-wrap-reverse xl:flex justify-between gap-x-8">
                {/* Text */}
                <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
                   <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
@@ -45,9 +45,32 @@ const Hero = () => {
                   />
                </div>
                {/* Image */}
-               <div className="hidden xl:flex relative">
+               <div className="pb-10 mx-auto xl:flex relative">
                   {/* Badge */}
-                  <Badge />
+                  <Badge
+                     containerStyles="absolute top-[24%] -left-[5rem]"
+                     icon={<RiBriefcase4Fill />}
+                     endCountNum={4}
+                     badgeText="years of Experience"
+                  />
+
+                  {/* Badge 2 */}
+                  <Badge
+                     containerStyles="absolute top-[80%] -left-[1rem]"
+                     icon={<RiTodoFill />}
+                     endCountNum={9}
+                     endCounttext="k"
+                     badgeText="Completeed Projects"
+                  />
+
+                  {/* Badge 3 */}
+                  <Badge
+                     containerStyles="absolute top-[55%] -right-8"
+                     icon={<RiTeamFill />}
+                     endCountNum={12}
+                     endCounttext="k"
+                     badgeText="Client Satisfied"
+                  />
                   <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2 "></div>
                   <DevImg
                      imgSrc="/about/developer.png"
