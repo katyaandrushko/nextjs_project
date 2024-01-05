2f6af1c787d772eb2cb2e25aea83af60497d1448
katyaandrushko <katyaandrushko14@gmail.com>
2024-01-05 17:37:17 +0200

    Add Tabs

--- components/About.jsx
+++ components/About.jsx
@@ -122,7 +122,59 @@ const About = () => {
    const getData = (arr, title) => {
       return arr.find((item) => item.title === title)
    }
-   return <div>About</div>
+   return (
+      <section className="xl:h-[860px] pb-12 xl:py-24">
+         <div className="container mx-auto ">
+            <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+               About me
+            </h2>
+            <div className="flex  flex-col xl:flex-row">
+               <div className="hidden xl:flex flex-1 relative">
+                  <DevImg
+                     containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
+                     imgSrc="/about/developer.png"
+                  />
+               </div>
+               {/* Tabs */}
+               <div className="flex-1">
+                  <Tabs defaultValue="personal">
+                     <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none ">
+                        <TabsTrigger
+                           value="personal"
+                           className="w-[162px] xl:w-auto"
+                        >
+                           Personal Info
+                        </TabsTrigger>
+                        <TabsTrigger
+                           value="qualifications"
+                           className="w-[162px] xl:w-auto"
+                        >
+                           Qualifications
+                        </TabsTrigger>
+                        <TabsTrigger
+                           value="skills"
+                           className="w-[162px] xl:w-auto"
+                        >
+                           Skills
+                        </TabsTrigger>
+                     </TabsList>
+
+                     {/* Tabs content */}
+                     <div className="text-lg mt-12 xl:mt-8">
+                        <TabsContent value="personal">
+                           Personal Info
+                        </TabsContent>
+                        <TabsContent value="qualifications">
+                           Qualifications
+                        </TabsContent>
+                        <TabsContent value="skills">Skills</TabsContent>
+                     </div>
+                  </Tabs>
+               </div>
+            </div>
+         </div>
+      </section>
+   )
 }
 
 export default About
